d9742aa47f4e8a92d0ba358f09135b6c1ed98a41
jiheeyu <j_yu@fanshaweonline.ca>
2020-10-23 03:54:11 +0900

    Async

--- js/main.js
+++ js/main.js
@@ -1,76 +1,42 @@
 //import your packages here
 import Team from "./modules/DataModule.js";
+import { fetchData } from "./modules/DataMiner.js";
 
 (() => {
-    // stub * just a place for non - component - specific stuff
+   
 
-    // set up the XMLHttp object
-    let myReq = new XMLHttpRequest;
-
-    //make sure we can handle whatever data comes back, or any errors
-    myReq.addEventListener("readystatechange",handleRequest);
-
-    // open a request and pass thr the URL of the data we want
-    myReq.open('GET','../DataSet.json');
-
-    // actually make the request
-    myReq.send();
-
-    // handleRequest function goes here
-    function handleRequest() {
-        if (myReq.readyState === XMLHttpRequest.DONE) {
-            // check status here and proceed
-            if (myReq.status === 200) {
-                // 200 means done and dusted, ready to go with the dataset!
-                handleDataSet(myReq.responseText);
-
-            } else {
-                // probably got some kind of error code, so handle that 
-                // a 404, 500 etc... can render appropriate error messages here
-                console.error(`${myReq.status} : something done broke, son`);
-            }
-        } else {
-            // request isn't ready yet, keep waiting...
-            console.log(`Request state: ${myReq.readyState}. Still processing...`);
-        }
-
-    }
-
-    let userSection = document.querySelector(".user-section"),
-        usertemplate = document.querySelector("#profs-template").content;
-            
+    //     .then(res => res.json()) // parse the JSON (translate) back to plain JS
+    //     .then(data => {
+    //         //this is our data (DataSet.json)
+    //         //converted to a plain Javascript object
+    //         handleDataSet(data);
+    //     })
+    // .ctach((error) => console.log(error));
     
+    // this receives the data payload from our AJAX request, parses it
     function handleDataSet(data) {
-        debugger;
+        let userSection = document.querySelector('.user-section'),
+            userTemplate = document.querySelector('#user-template').content;
 
-        for (let user in Team) {
-
-            debugger;
-            //make a copy of our template here and then
-            //populate the children (text elements) with
-            //the static data from the Team object
-            
-            let currentUser = usertemplate.cloneNode(true),
-                currentUserText = currentUser.querySelector(".user").children;
-
-            currentUserText[1].textContent = data[user].name;
+        //loop through
+        for (let user in data) {
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.user').children;
+                debugger;
+            currentUserText[1].src = `./images/${data[user].avatar}.jpg`;
             currentUserText[2].textContent = data[user].role;
-            currentUserText[3].textContent = data[user].nickname;
+            currentUserText[3].textContent = data[user].name;
+            currentUserText[4].textContent = data[user].nickname;
 
+
+            // add this new user to the view
             userSection.appendChild(currentUser);
         }
-
-
-        // userSection[1].textContent = Team["Justin"].name;
-        // userSection[2].textContent = Team["Justin"].role;
-        // userSection[3].textContent = Team["Justin"].nickname;
-    }
-   
-    // debugger;
-    // select our user elements and load the content
-    // handleDateSt(Team);
-
-    // userSection[1].textContent = Team["Justin"].name;
-    // userSection[2].textContent = Team["Justin"].role;
-    // userSection[3].textContent = Team["Justin"].nickname;
+        console.log(data);
+    } 
+    fetchData('./DataSet.json').then(data => handleDataSet(data)).catch(err => console.log(err));
+    etchData('./AnotherDataSet.json').then(data => handleDataSet(data)).catch(err => console.log(err));
+    
 })();
+
+    
